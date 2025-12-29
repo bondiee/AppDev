d43360e4a1b85cd81944d61b18e4abdd560dc32d
Kurt Borondia <kurtborondiaa@gmail.com>
2025-12-29 11:33:26 +0800

    Add project files.

---
+++ main/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int age = 20;
+        Console.WriteLine("My name is Kurt from BSCS 3-2!");
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("This Repository is for my AppDev Requirements!");
+    }
+}
